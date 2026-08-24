4932d10cb83646c59ce6fb447a3f9be8f3b39b42
yyy72ys <yohey9672@gmail.com>
2026-08-24 20:57:27 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
-// Top-level build file
+// Top-level build file - compose plugin removed for Kotlin 1.9 compatibility
 plugins {
     id("com.android.application") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.22" apply false
 }
