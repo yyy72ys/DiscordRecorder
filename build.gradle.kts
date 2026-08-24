13cd2055ec03c7a221f9f8c52385d0f0e9aab05e
Builder <builder@example.com>
2026-08-24 21:13:13 +0900

    fix: bump Kotlin to 1.9.23 and Compose compiler to 1.5.14 for compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file - compose plugin removed for Kotlin 1.9 compatibility
 plugins {
     id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
